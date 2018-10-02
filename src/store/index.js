f61e50fe3fca7fefdc98375486ef4b575cc2e9a5
Srikanth Gowda <srikanthgowdae@gmail.com>
2018-10-02 22:52:11 +0530

    changes added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose } from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import { addTeamMember } from './actions/member';
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const devtools = process.env.NODE_ENV === 'test'
+? x => x /* eslint-disable no-underscore-dangle */
+: window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
+/* eslint-enable no-underscore-dangle */
+
+const store = createStore(rootReducer,compose(applyMiddleware(thunk),devtools));
 store.subscribe(() => console.log(store.getState()));
 export default store;
 
