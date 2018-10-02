de4fa1099750765bd7ec617532c9b998af31eb32
Srikanth Gowda <srikanthgowdae@gmail.com>
2018-10-02 22:36:06 +0530

    Updates

---
+++ precache-manifest.ddef0e346ac220ca5cde156dc618fcbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "568fc5bd3f3deab1b2ce911f51414f51",
+    "url": "/employee-management-app/static/media/sri.568fc5bd.jpg"
+  },
+  {
+    "revision": "00a7330a2eb271af7a01",
+    "url": "/employee-management-app/static/js/runtime~main.00a7330a.js"
+  },
+  {
+    "revision": "b887e1ac3c2dc93d739e",
+    "url": "/employee-management-app/static/js/main.b887e1ac.chunk.js"
+  },
+  {
+    "revision": "e32c2a6cd95bc1a88383",
+    "url": "/employee-management-app/static/js/1.e32c2a6c.chunk.js"
+  },
+  {
+    "revision": "b887e1ac3c2dc93d739e",
+    "url": "/employee-management-app/static/css/main.542bb3c8.chunk.css"
+  },
+  {
+    "revision": "e32c2a6cd95bc1a88383",
+    "url": "/employee-management-app/static/css/1.896b677a.chunk.css"
+  },
+  {
+    "revision": "f8023b349a1d868353e15eb3c9b5dbe2",
+    "url": "/employee-management-app/index.html"
+  }
+];
